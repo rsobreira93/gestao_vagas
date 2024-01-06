8db968868bcdb897bc10174951693a1f73a53885
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-06 10:43:38 -0300

    feat: add create job dto

---
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package br.com.sobreiraromulo.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+
+}
