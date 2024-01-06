b1d8b538bf79eb7aa9217e6672e41059aa359355
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-06 10:45:05 -0300

    feat: add create job dto and modify create method in jobcontroller

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,13 +1,17 @@
 package br.com.sobreiraromulo.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sobreiraromulo.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.sobreiraromulo.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.sobreiraromulo.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,7 +22,16 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
-  public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    var jobEntity = JobEntity.builder()
+        .description(createJobDTO.getDescription())
+        .benefits(createJobDTO.getBenefits())
+        .level(createJobDTO.getLevel())
+        .companyId(UUID.fromString(companyId.toString()))
+        .build();
+
     return createJobUseCase.execute(jobEntity);
   }
 }
