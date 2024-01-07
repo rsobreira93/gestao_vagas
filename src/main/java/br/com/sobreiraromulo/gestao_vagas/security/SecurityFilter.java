1800ce8693752183af90d951bfab0bea03e231b9
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 11:26:54 -0300

    refactor: change security filter to handle company requests

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/security/SecurityFilter.java
@@ -28,18 +28,20 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     String header = request.getHeader("Authorization");
 
-    if (header != null) {
-      var subjectToken = this.jwtProvider.validateToken(header);
+    if (request.getRequestURI().startsWith("/companies")) {
+      if (header != null) {
+        var subjectToken = this.jwtProvider.validateToken(header);
 
-      if (subjectToken.isEmpty()) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return;
+        if (subjectToken.isEmpty()) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+
+        request.setAttribute("company_id", subjectToken);
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
+            Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(auth);
       }
-
-      request.setAttribute("company_id", subjectToken);
-      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
-          Collections.emptyList());
-      SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
     filterChain.doFilter(request, response);
