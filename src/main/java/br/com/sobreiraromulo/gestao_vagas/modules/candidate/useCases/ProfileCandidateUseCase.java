a91520acf150d5b8e36fa1b54f0473f07f36ccfb
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 13:29:05 -0300

    style: remove unused import

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.sobreiraromulo.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 
