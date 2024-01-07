f2cfb36a414b21932059c15cc11e8c92b9975dc4
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 16:02:21 -0300

    refactor: remove swagger annotations

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/GestaoVagasApplication.java
@@ -3,11 +3,7 @@ package br.com.sobreiraromulo.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", version = "1.0", description = "API para gestão de vagas de emprego"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
