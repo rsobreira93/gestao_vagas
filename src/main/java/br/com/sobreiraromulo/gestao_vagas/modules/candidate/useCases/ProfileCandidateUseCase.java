5f6ceb4476a659b4a9091bda4dabeb7f4e24a3d9
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 09:34:24 -0300

    feat: add ProfileCandidateUseCase implementation

---
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,36 @@
+package br.com.sobreiraromulo.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+
+@Service
+public class ProfileCandidateUseCase {
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+    var candidate = this.candidateRepository.findById(idCandidate)
+        .orElseThrow(() -> {
+          throw new UsernameNotFoundException("User not found");
+        });
+
+    var candidateResponseDTO = ProfileCandidateResponseDTO.builder()
+        .id(candidate.getId())
+        .name(candidate.getName())
+        .email(candidate.getEmail())
+        .username(candidate.getUsername())
+        .description(candidate.getDescription())
+        .build();
+
+    return candidateResponseDTO;
+
+  }
+}
