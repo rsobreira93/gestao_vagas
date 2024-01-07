dce7c1f7444951ad5d81f258cbf056fe545cfb7e
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 08:25:30 -0300

    feat: add expiration time to jwt token generation

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -42,16 +42,19 @@ public class AuthCandidateUseCase {
 
     Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
 
+    var expires_in = Instant.now().plus(Duration.ofMinutes(10));
+
     var token = JWT.create()
         .withIssuer("javagas")
         .withSubject(candidate.getId().toString())
         .withClaim("roles", Arrays.asList("candidate"))
-        .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+        .withExpiresAt(expires_in)
         .sign(algorithm);
 
     var authCandidateResponse = AuthCandidateResponseDTO
         .builder()
         .access_token(token)
+        .expires_in(expires_in.toEpochMilli())
         .build();
 
     return authCandidateResponse;
