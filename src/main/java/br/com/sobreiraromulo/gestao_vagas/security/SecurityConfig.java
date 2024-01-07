a6461b4d774c3e61ea76a77c2ae0017ece8cfe43
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-07 11:27:20 -0300

    refactor: update security filters

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/security/SecurityConfig.java
@@ -15,18 +15,22 @@ public class SecurityConfig {
   @Autowired
   private SecurityFilter securityFilter;
 
+  @Autowired
+  private SecurityCandidateFilter securityCandidateFilter;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers("/candidates/").permitAll()
               .requestMatchers("/companies/").permitAll()
-              .requestMatchers("/auth/companies").permitAll()
+              .requestMatchers("/companies/auth").permitAll()
               .requestMatchers("/candidates/auth").permitAll();
 
           auth.anyRequest().authenticated();
         })
-        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
     ;
     return http.build();
