21eed93fc936cc923d57310e21b88b19d2766abf
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-13 09:06:36 -0300

    feat: add Apply job candidate

---
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,34 @@
+package br.com.sobreiraromulo.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.sobreiraromulo.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.sobreiraromulo.gestao_vagas.exceptions.UserFoundException;
+import br.com.sobreiraromulo.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.sobreiraromulo.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @Autowired
+  private JobRepository jobRepository;
+
+  public void execute(UUID idCandidate, UUID idJob) {
+    var candidate = this.candidateRepository.findById(idCandidate)
+        .orElseThrow(() -> {
+          throw new UserNotFoundException();
+        });
+
+    var job = this.jobRepository.findById(idJob)
+        .orElseThrow(() -> {
+          throw new JobNotFoundException();
+        });
+  }
+}
