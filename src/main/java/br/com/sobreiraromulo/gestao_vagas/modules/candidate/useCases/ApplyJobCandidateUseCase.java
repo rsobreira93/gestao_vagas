753ad8ad91b8796e8fae4b025060c9d4c6c009bf
Romulo Sobreira <sobreiraromuloes@gmail.com>
2024-01-15 08:30:18 -0300

    feat: add ApplyJobEntityUseCase

--- src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/sobreiraromulo/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import br.com.sobreiraromulo.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.sobreiraromulo.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.sobreiraromulo.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.entity.ApplyJobEntity;
+import br.com.sobreiraromulo.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.sobreiraromulo.gestao_vagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -19,15 +21,28 @@ public class ApplyJobCandidateUseCase {
   @Autowired
   private JobRepository jobRepository;
 
-  public void execute(UUID idCandidate, UUID idJob) {
-    var candidate = this.candidateRepository.findById(idCandidate)
+  @Autowired
+  private ApplyJobRepository applyJobRepository;
+
+  public ApplyJobEntity execute(UUID idCandidate, UUID idJob) {
+    this.candidateRepository.findById(idCandidate)
         .orElseThrow(() -> {
           throw new UserNotFoundException();
         });
 
-    var job = this.jobRepository.findById(idJob)
+    this.jobRepository.findById(idJob)
         .orElseThrow(() -> {
           throw new JobNotFoundException();
         });
+
+    var applyJob = ApplyJobEntity.builder()
+        .candidateId(idCandidate)
+        .jobId(idJob)
+        .build();
+
+    applyJob = this.applyJobRepository.save(applyJob);
+
+    return applyJob;
   }
+
 }
